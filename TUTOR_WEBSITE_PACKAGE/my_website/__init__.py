0eeb4badf441ae501b799205ec61b3e7250cf052
Jsparkzprime <jsparkzprime@gmail.com>
2024-03-05 17:26:55 +0000

    Modified Signup Page

--- TUTOR_WEBSITE_PACKAGE/my_website/__init__.py
+++ TUTOR_WEBSITE_PACKAGE/my_website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET KEY'] = 'oLaJiDeMaBoGuNjE'
+    app.secret_key = 'oLaJiDeMaBoGuNjE'
 
     from .views import views
     from .auth import auth
--- TUTOR_WEBSITE_PACKAGE/my_website/auth.py
+++ TUTOR_WEBSITE_PACKAGE/my_website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -14,4 +14,22 @@ def logout():
 
 @auth.route('/sign-up', methods = ['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        fullname = request.form.get('fullname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) <= 5:
+            flash('Email address must be greater than 5 characters', category='error')
+        elif len(email) > 254:
+            flash('Email address must be less than 255 characters', category='error')
+        elif len(fullname) > 70:
+            flash('Fullname must be less than 70 characters', category='error')
+        elif password1 != password2:
+            flash("Passwords do not match", category='error')
+        elif len(password1) < 8:
+            flash("Password must be a minimum of 8 characters", category='error')
+        else: 
+            flash('You have created an account', category= 'success')
     return render_template("sign_up.html")
